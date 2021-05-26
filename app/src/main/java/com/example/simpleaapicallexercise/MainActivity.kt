8d87581affbc46b0cacf4b1346ed1172ff97f30a
Beratcan Güreş <guresberatcan@gmail.com>
2021-05-26 18:56:00 +0300

    Getting data from json completed

--- app/src/main/java/com/example/simpleaapicallexercise/MainActivity.kt
+++ app/src/main/java/com/example/simpleaapicallexercise/MainActivity.kt
@@ -1,11 +1,92 @@
 package com.example.simpleaapicallexercise
 
+import android.app.Dialog
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        CallAPILoginAsyncTask().execute()
     }
+    private inner class CallAPILoginAsyncTask(): AsyncTask<Any, Void, String>(){
+
+        private lateinit var customProgressDialog: Dialog
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+             showProgressDialog()
+
+        }
+
+        override fun doInBackground(vararg params: Any?): String {
+            var result: String
+            var connection: HttpURLConnection? = null
+            try {
+                var url= URL("https://run.mocky.io/v3/f1769bf0-3b02-4d13-a248-47b42a70485b")
+                connection = url.openConnection() as HttpURLConnection
+                connection.doInput = true
+                connection.doOutput = true
+                val httpResult : Int = connection.responseCode
+                if(httpResult == HttpURLConnection.HTTP_OK){
+                    val inputStream = connection.inputStream
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+                    val stringBuilder = StringBuilder()
+                    var line: String?
+                    try{
+                        while(reader.readLine().also { line=it } != null){
+                            stringBuilder.append(line+"\n")
+                        }
+                    }catch (e : IOException){
+                        e.printStackTrace()
+                    }finally {
+                        try{
+                            inputStream.close()
+                        }catch (e: IOException){
+                            e.printStackTrace()
+                        }
+                    }
+                    result = stringBuilder.toString()
+                }else{
+                    result = connection.responseMessage
+                }
+            }catch (e: SocketTimeoutException){
+                result = "Connection Timeout"
+            }catch (e: Exception){
+                result = "Error : " + e.message
+            }finally {
+                connection?.disconnect()
+            }
+            return result
+        }
+
+
+        private fun showProgressDialog(){
+            customProgressDialog = Dialog(this@MainActivity)
+            customProgressDialog.setContentView(R.layout.dialog_custom_progress)
+            customProgressDialog.show()
+        }
+
+        private fun cancelProgressDialog(){
+            customProgressDialog.dismiss()
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            cancelProgressDialog()
+
+            Log.i("JSON RESPONSE RESULT",result!!)
+        }
+    }
+
 }
